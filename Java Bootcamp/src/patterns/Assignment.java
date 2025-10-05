dd929da9c48335e2c877a88a49b685be7a293ea2
Tejas Bansal <tejasbansal6065@gmail.com>
2025-10-06 00:07:18 +0530

    pattern 9

--- Java Bootcamp/src/patterns/Assignment.java
+++ Java Bootcamp/src/patterns/Assignment.java
@@ -82,7 +82,17 @@ public class Assignment {
 //            System.out.println();
 //        }
 
-
+        // Pattern 9
+        for(int i=n;i>0;i--){
+            int totalSpaces = n-i;
+            for(int j=0;j<totalSpaces;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
