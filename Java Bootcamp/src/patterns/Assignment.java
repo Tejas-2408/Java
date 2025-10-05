ee7b002fdd06b64b9c094ae2b28178cb87c81c63
Tejas Bansal <tejasbansal6065@gmail.com>
2025-10-06 00:10:34 +0530

    pattern 10 and 11

--- Java Bootcamp/src/patterns/Assignment.java
+++ Java Bootcamp/src/patterns/Assignment.java
@@ -83,13 +83,37 @@ public class Assignment {
 //        }
 
         // Pattern 9
+//        for(int i=n;i>0;i--){
+//            int totalSpaces = n-i;
+//            for(int j=0;j<totalSpaces;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<2*i-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // Pattern 10
+//        for(int i=1;i<=n;i++){
+//            int totalSpaces = n-i;
+//            for(int j=0;j<totalSpaces;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        // Pattern 11
         for(int i=n;i>0;i--){
             int totalSpaces = n-i;
             for(int j=0;j<totalSpaces;j++){
                 System.out.print(" ");
             }
-            for(int j=0;j<2*i-1;j++){
-                System.out.print("*");
+            for(int j=0;j<i;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
