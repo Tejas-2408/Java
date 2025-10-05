56b841e770563a7865641453f79340aa063a740d
Tejas Bansal <tejasbansal6065@gmail.com>
2025-10-05 23:41:51 +0530

    patterns

--- Java Bootcamp/src/patterns/Assignment.java
+++ Java Bootcamp/src/patterns/Assignment.java
@@ -12,5 +12,13 @@ public class Assignment {
 //            }
 //            System.out.println();
 //        }
+
+        // Pattern 2
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
