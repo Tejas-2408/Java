61129ccd6842281971e32fa30953e4e09c4f9be7
Tejas Bansal <tejasbansal6065@gmail.com>
2025-10-06 10:54:01 +0530

    pattern 14

--- Java Bootcamp/src/patterns/Assignment.java
+++ Java Bootcamp/src/patterns/Assignment.java
@@ -150,5 +150,21 @@ public class Assignment {
 //        }
 
         // Pattern 14
+//        for(int i=n;i>0;i--){
+//            for(int j=0;j<n-i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=1;j<2*i;j++){
+//                if(j==1 || j==(2*i)-1 || i==n){
+//                    System.out.print("*");
+//                }
+//                else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+        // pattern 15
     }
 }
