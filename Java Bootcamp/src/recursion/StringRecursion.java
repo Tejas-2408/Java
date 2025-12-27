7ffbd4b626e6b7687986a72a57b7a68848be188b
Tejas Bansal <tejasbansal6065@gmail.com>
2025-12-27 12:16:17 +0530

    String recusion

--- Java Bootcamp/src/recursion/StringRecursion.java
+++ Java Bootcamp/src/recursion/StringRecursion.java
@@ -5,6 +5,8 @@ public class StringRecursion {
         String s = "bacaccad";
         System.out.println(removeAArgument(s,0,""));
         System.out.println(removeANewString(s,0));
+        skipA("",s);
+        System.out.println(skipApple("dfgappleqza"));
     }
 
     static String removeAArgument(String s, int index, String ans){
@@ -27,5 +29,33 @@ public class StringRecursion {
         String ans = s.charAt(index) != 'a' ? s.charAt(index)+"" : "";
         return ans + removeANewString(s,index+1);
 
+    }
+
+    static void skipA(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skipA(p,up.substring(1));
+        }
+        else{
+            skipA(p+ch,up.substring(1));
+        }
+    }
+
+    static String skipApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch = up.charAt(0);
+        if(up.startsWith("apple")){
+            return skipApple(up.substring(5));
+        }
+        else{
+            return ch + skipApple(up.substring(1));
+        }
     }
 }
