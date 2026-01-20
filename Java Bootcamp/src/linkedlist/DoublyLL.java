e47c69deac03a8c9f64d1450b4965aa75ea76a4c
Tejas Bansal <tejasbansal6065@gmail.com>
2026-01-21 01:12:17 +0530

    insert elements in doubly ll

--- Java Bootcamp/src/linkedlist/DoublyLL.java
+++ Java Bootcamp/src/linkedlist/DoublyLL.java
@@ -10,6 +10,63 @@ public class DoublyLL {
         this.size = 0;
     }
 
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        if(size==0){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+        size++;
+    }
+
+    public void insertLast(int val){
+        Node node = new Node(val);
+        if(tail == null){
+            insertFirst(val);
+            return;
+        }
+
+        node.prev = tail.next;
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int val, int index){
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+
+        if(index == size){
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+        for(int i = 1 ; i < index ; i++){
+            temp = temp.next;
+        }
+
+        Node node = new Node(val,temp.next,temp);
+        temp.next = node;
+        node.next.prev = node;
+        size++;
+    }
+
+    public void display(){
+        Node node = head;
+        while(node != null){
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.println("END");
+    }
+
 
 
     private class Node{
--- Java Bootcamp/src/linkedlist/Main.java
+++ Java Bootcamp/src/linkedlist/Main.java
@@ -7,20 +7,32 @@ public class Main {
         LinkedList<Integer> ll = new LinkedList<>();
         ll.add(24);
 
-        LL ll1 = new LL();
-        ll1.insertFirst(3);
-        ll1.insertFirst(4);
-        ll1.insertFirst(5);
-        ll1.insertFirst(6);
-        ll1.insertLast(2);
-        ll1.insertLast(1);
-        ll1.insert(14,3);
-        ll1.display();
-        ll1.deleteFirst();
-        ll1.display();
-        ll1.deleteLast();
-        ll1.display();
-        ll1.delete(2);
-        ll1.display();
+//        LL ll1 = new LL();
+//        ll1.insertFirst(3);
+//        ll1.insertFirst(4);
+//        ll1.insertFirst(5);
+//        ll1.insertFirst(6);
+//        ll1.insertLast(2);
+//        ll1.insertLast(1);
+//        ll1.insert(14,3);
+//        ll1.display();
+//        ll1.deleteFirst();
+//        ll1.display();
+//        ll1.deleteLast();
+//        ll1.display();
+//        ll1.delete(2);
+//        ll1.display();
+
+        DoublyLL dll = new DoublyLL();
+        dll.insertFirst(5);
+        dll.insertFirst(3);
+        dll.insertFirst(8);
+        dll.insertFirst(10);
+        dll.insertFirst(1);
+        dll.insertLast(2);
+        dll.insertLast(3);
+        dll.display();
+        dll.insert(24,1);
+        dll.display();
     }
 }
