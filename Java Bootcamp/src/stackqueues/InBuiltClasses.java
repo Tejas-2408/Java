6d7fc339ac23cc0d356fdd68cf17b7ba60026a07
Tejas Bansal <tejasbansal6065@gmail.com>
2026-01-29 23:17:29 +0530

    in built queue

--- Java Bootcamp/src/stackqueues/InBuiltClasses.java
+++ Java Bootcamp/src/stackqueues/InBuiltClasses.java
@@ -1,5 +1,7 @@
 package stackqueues;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class InBuiltClasses {
@@ -10,11 +12,27 @@ public class InBuiltClasses {
         stack.push(3);
         stack.push(4);
         stack.push(5);
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop()); this will give error as atack is empty
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        System.out.println(queue.peek());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+
+
     }
 }
