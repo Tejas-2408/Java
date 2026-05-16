dc636d1e13a4c5933ba53817ab39385d01325751
Tejas Bansal <tejasbansal6065@gmail.com>
2026-05-16 11:36:41 +0530

    Binary tree

---
+++ Java Bootcamp/src/tree/BinaryTree.java
@@ -0,0 +1,71 @@
+package tree;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class BinaryTree {
+
+    public BinaryTree() {
+    }
+
+    private static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private Node root;
+
+    // insert elements
+    public void populate(Scanner scanner){
+        System.out.print("Enter the root value: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner,root);
+    }
+
+    private void populate(Scanner scanner, Node node) {
+        System.out.println("Do you want to enter left of "+ node.value);
+        boolean left = scanner.nextBoolean();
+        if(left){
+            System.out.print("Enter the value of the left of " + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner,node.left);
+        }
+
+        System.out.println("Do you want to enter right of "+ node.value);
+        boolean right = scanner.nextBoolean();
+        if(right){
+            System.out.print("Enter the value of the left of " + node.value + " ");
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner,node.right);
+        }
+    }
+
+    public void display(){
+        display(root,"");
+    }
+
+    private void display(Node node, String indent) {
+        if(node == null){
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left,indent + "\t");
+        display(node.right,indent + "\t");
+    }
+
+
+}
