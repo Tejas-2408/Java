4e70ec566843ae383e69d44157980a9e4b506f39
Tejas Bansal <tejasbansal6065@gmail.com>
2026-05-24 12:11:57 +0530

    Generate Random String

---
+++ Java Bootcamp/src/string/buffer/RandomString.java
@@ -0,0 +1,22 @@
+package string.buffer;
+
+import java.util.Random;
+
+public class RandomString {
+
+    static String generate(int size){
+        StringBuffer sb = new StringBuffer(size);
+        Random random = new Random();
+        for(int i = 0; i < size; i++){
+            int randomChar = 97 + (int)(random.nextFloat()*26);
+            sb.append((char)randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(20));
+
+    }
+}
