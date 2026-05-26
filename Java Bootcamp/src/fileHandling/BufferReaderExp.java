fee2e109ccca61081d42eff644bb61543ee60578
Tejas Bansal <tejasbansal6065@gmail.com>
2026-05-26 19:24:33 +0530

    Buffered reader

---
+++ Java Bootcamp/src/fileHandling/BufferReaderExp.java
@@ -0,0 +1,25 @@
+package fileHandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferReaderExp {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            System.out.println("You typed: " + br.readLine());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader("D:\\Java\\Java Bootcamp\\src\\fileHandling\\note.txt"))){
+            while(br.ready()){
+                System.out.println(br.readLine());
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
